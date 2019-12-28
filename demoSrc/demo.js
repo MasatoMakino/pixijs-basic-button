dbed354eb7335087f9b57e2fa8b9ea766d8b0506
MasatoMakino <unagiinu.g@gmail.com>
2019-12-28 16:39:35 +0900

    add : radio button demo.

--- demoSrc/demo.js
+++ demoSrc/demo.js
@@ -1,7 +1,9 @@
 import {
+  BasicRadioButton,
   BasicClickButton,
   BasicCheckButton,
-  BasicButtonEventType
+  BasicButtonEventType,
+  BasicRadioButtonManager
 } from "../bin/index";
 import { Application, Graphics } from "pixi.js";
 
@@ -13,49 +15,58 @@ const onDomContentsLoaded = () => {
   const app = new Application({ width: 640, height: 480 });
   document.body.appendChild(app.view);
 
-  const addLabel = btn => {
-    btn.addLabel(
-      0,
-      0,
-      "Abcdefghijklあ",
-      { fontSize: 16 },
-      {
-        normal: 0xffffff,
-        over: 0xff00ff,
-        down: 0x000000,
-        selectNormal: 0xffff33,
-        selectOver: 0x00ff00,
-        selectDown: 0x999999
-      }
-    );
-  };
-  const button = new BasicClickButton({
-    normal: getRect(0xff00ff),
-    over: getRect(0xff66ff),
-    down: getRect(0xffffff)
-  });
+  initButton(app.stage);
+  initCheckButton(app.stage);
+  initRadioButton(app.stage);
+};
+
+const initButton = stage => {
+  const button = new BasicClickButton(getMaterialSet());
   button.x = button.y = 36;
   addLabel(button);
+  stage.addChild(button);
+};
 
-  const buttonSelect = new BasicCheckButton({
+const initCheckButton = stage => {
+  const buttonSelect = new BasicCheckButton(getMaterialSet());
+  buttonSelect.y = 36;
+  buttonSelect.x = 36 + 128 + 36;
+  addLabel(buttonSelect);
+  buttonSelect.buttonValue = "__button__val__test";
+
+  stage.addChild(buttonSelect);
+
+  buttonSelect.on(BasicButtonEventType.SELECTED, e => {
+    console.log(e);
+  });
+};
+
+const initRadioButton = stage => {
+  const manager = new BasicRadioButtonManager();
+
+  const n = 4;
+  for (let i = 0; i < n; i++) {
+    const buttonRadio = new BasicRadioButton(getMaterialSet());
+    buttonRadio.y = 128;
+    buttonRadio.x = 36 + (128 + 36) * i;
+    addLabel(buttonRadio);
+    buttonRadio.buttonValue = "__button__val__test__" + i;
+    stage.addChild(buttonRadio);
+    manager.add(buttonRadio);
+  }
+
+  manager.selected = manager.buttons[0];
+};
+
+const getMaterialSet = () => {
+  return {
     normal: getRect(0xff00ff),
     over: getRect(0xff66ff),
     down: getRect(0xffffff),
     selectNormal: getRect(0x330033),
     selectOver: getRect(0x442244),
     selectDown: getRect(0x333333)
-  });
-  buttonSelect.y = 36;
-  buttonSelect.x = 36 + 128 + 36;
-  addLabel(buttonSelect);
-  buttonSelect.buttonValue = "__button__val__test";
-
-  app.stage.addChild(button);
-  app.stage.addChild(buttonSelect);
-
-  buttonSelect.on(BasicButtonEventType.SELECTED, e => {
-    console.log(e);
-  });
+  };
 };
 
 const getRect = color => {
@@ -65,6 +76,23 @@ const getRect = color => {
     .drawRect(0, 0, 128, 32)
     .endFill();
   return gra;
+};
+
+const addLabel = btn => {
+  btn.addLabel(
+    0,
+    0,
+    "Abcdefghijklあ",
+    { fontSize: 16 },
+    {
+      normal: 0xffffff,
+      over: 0xff00ff,
+      down: 0x000000,
+      selectNormal: 0xffff33,
+      selectOver: 0x00ff00,
+      selectDown: 0x999999
+    }
+  );
 };
 
 /**
