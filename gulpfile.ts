3396e1f41185712aceda89ed77c5d80987b2394c
MasatoMakino <unagiinu.g@gmail.com>
2020-07-25 14:33:27 +0900

    change : separate tsconfig for commonjs .

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,11 +3,11 @@ const { parallel, series } = require("gulp");
 const doc = require("gulptask-tsdoc").get();
 const server = require("gulptask-dev-server").get("./docs/demo");
 const { bundleDemo, watchDemo } = require("gulptask-demo-page").get({
-  externalScripts: []
+  externalScripts: [],
 });
 
 const { tsc, tscClean, watchTsc } = require("gulptask-tsc").get({
-  projects: ["tsconfig.json", "tsconfig.esm.json"]
+  projects: ["tsconfig.cjs.json", "tsconfig.esm.json"],
 });
 
 const watchTasks = async () => {
