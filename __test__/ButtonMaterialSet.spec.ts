2a0c1c52d53923883f30b5aab853161b9f55c6a8
MasatoMakino <unagiinu.g@gmail.com>
2020-07-26 11:46:31 +0900

    update : test for ButtonMaterialSet

--- __test__/ButtonMaterialSet.spec.ts
+++ __test__/ButtonMaterialSet.spec.ts
@@ -1,5 +1,8 @@
 import { BasicButtonState, ButtonMaterialSet } from "../src";
-import { getTestMaterialSet } from "./TestMaterial";
+import {
+  getTestMaterialSet,
+  getTestMaterialSetNormalOnly,
+} from "./TestMaterial";
 
 describe("ButtonMaterialSet", () => {
   const mat = getTestMaterialSet();
@@ -11,9 +14,56 @@ describe("ButtonMaterialSet", () => {
   test("change state", () => {
     ButtonMaterialSet.updateVisible(mat, BasicButtonState.NORMAL);
     testMaterialVisible(mat, BasicButtonState.NORMAL);
-
     ButtonMaterialSet.updateVisible(mat, BasicButtonState.NORMAL_OVER);
     testMaterialVisible(mat, BasicButtonState.NORMAL_OVER);
+    ButtonMaterialSet.updateVisible(mat, BasicButtonState.NORMAL_DOWN);
+    testMaterialVisible(mat, BasicButtonState.NORMAL_DOWN);
+
+    ButtonMaterialSet.updateVisible(mat, BasicButtonState.SELECT);
+    testMaterialVisible(mat, BasicButtonState.SELECT);
+    ButtonMaterialSet.updateVisible(mat, BasicButtonState.SELECT_OVER);
+    testMaterialVisible(mat, BasicButtonState.SELECT_OVER);
+    ButtonMaterialSet.updateVisible(mat, BasicButtonState.SELECT_DOWN);
+    testMaterialVisible(mat, BasicButtonState.SELECT_DOWN);
+
+    ButtonMaterialSet.updateVisible(mat, BasicButtonState.DISABLE);
+    testMaterialVisible(mat, BasicButtonState.DISABLE);
+
+    ButtonMaterialSet.updateVisible(mat, "unexpected value" as any);
+    testMaterialVisible(mat, BasicButtonState.NORMAL);
+  });
+
+  test("change state : normal only", () => {
+    const matNormalOnly = getTestMaterialSetNormalOnly();
+
+    ButtonMaterialSet.updateVisible(matNormalOnly, BasicButtonState.NORMAL);
+    expect(matNormalOnly.normal.visible).toBe(true);
+    ButtonMaterialSet.updateVisible(
+      matNormalOnly,
+      BasicButtonState.NORMAL_OVER
+    );
+    expect(matNormalOnly.normal.visible).toBe(true);
+    ButtonMaterialSet.updateVisible(
+      matNormalOnly,
+      BasicButtonState.NORMAL_DOWN
+    );
+    expect(matNormalOnly.normal.visible).toBe(true);
+
+    ButtonMaterialSet.updateVisible(matNormalOnly, BasicButtonState.DISABLE);
+    expect(matNormalOnly.normal.visible).toBe(true);
+
+    ButtonMaterialSet.updateVisible(matNormalOnly, BasicButtonState.SELECT);
+    expect(matNormalOnly.normal.visible).toBe(true);
+    ButtonMaterialSet.updateVisible(
+      matNormalOnly,
+      BasicButtonState.SELECT_OVER
+    );
+    expect(matNormalOnly.normal.visible).toBe(true);
+    ButtonMaterialSet.updateVisible(
+      matNormalOnly,
+      BasicButtonState.SELECT_DOWN
+    );
+    expect(matNormalOnly.normal.visible).toBe(true);
   });
 });
 
@@ -22,11 +72,11 @@ const testMaterialVisible = (
   state: BasicButtonState
 ) => {
   expect(mat.normal.visible).toBe(state === BasicButtonState.NORMAL);
-  expect(mat.over.visible).toBe(state === BasicButtonState.NORMAL_OVER);
-  expect(mat.down.visible).toBe(state === BasicButtonState.NORMAL_DOWN);
-  expect(mat.disable.visible).toBe(state === BasicButtonState.DISABLE);
+  expect(mat.over?.visible).toBe(state === BasicButtonState.NORMAL_OVER);
+  expect(mat.down?.visible).toBe(state === BasicButtonState.NORMAL_DOWN);
+  expect(mat.disable?.visible).toBe(state === BasicButtonState.DISABLE);
 
-  expect(mat.selectNormal.visible).toBe(state === BasicButtonState.SELECT);
-  expect(mat.selectDown.visible).toBe(state === BasicButtonState.SELECT_DOWN);
-  expect(mat.selectOver.visible).toBe(state === BasicButtonState.SELECT_OVER);
+  expect(mat.selectNormal?.visible).toBe(state === BasicButtonState.SELECT);
+  expect(mat.selectDown?.visible).toBe(state === BasicButtonState.SELECT_DOWN);
+  expect(mat.selectOver?.visible).toBe(state === BasicButtonState.SELECT_OVER);
 };
--- __test__/TestMaterial.ts
+++ __test__/TestMaterial.ts
@@ -5,31 +5,39 @@ import { ButtonMaterialSet } from "../src";
  * テスト用の共通マテリアルを生成する
  */
 
-export enum TestMaterialAlpha {
-  normal = 1.0,
-  down = 0.91,
-  over = 0.92,
-  disable = 0.93,
-  selectNormal = 0.8,
-  selectDown = 0.81,
-  selectOver = 0.82,
+export enum TestMaterialColor {
+  normal,
+  down,
+  over,
+  disable,
+  selectNormal,
+  selectDown,
+  selectOver,
+  selectMarker,
 }
-export function getTestMaterialSet() {
+
+export function getTestMaterialSetNormalOnly() {
   const mat = new ButtonMaterialSet();
+  mat.normal = getRect(TestMaterialColor.normal);
+  return mat;
+}
 
-  mat.normal = getRect(0x00ff00, TestMaterialAlpha.normal);
-  mat.down = getRect(0x00ff00, TestMaterialAlpha.down);
-  mat.over = getRect(0x00ff00, TestMaterialAlpha.over);
-  mat.disable = getRect(0x00ff00, TestMaterialAlpha.disable);
+export function getTestMaterialSet() {
+  const mat = getTestMaterialSetNormalOnly();
+  mat.down = getRect(TestMaterialColor.down);
+  mat.over = getRect(TestMaterialColor.over);
+  mat.disable = getRect(TestMaterialColor.disable);
 
-  mat.selectNormal = getRect(0x00ff00, TestMaterialAlpha.selectNormal);
-  mat.selectDown = getRect(0x00ff00, TestMaterialAlpha.selectDown);
-  mat.selectOver = getRect(0x00ff00, TestMaterialAlpha.selectOver);
+  mat.selectNormal = getRect(TestMaterialColor.selectNormal);
+  mat.selectDown = getRect(TestMaterialColor.selectDown);
+  mat.selectOver = getRect(TestMaterialColor.selectOver);
+
+  mat.selectMarker = getRect(TestMaterialColor.selectMarker);
 
   return mat;
 }
 
-function getRect(color: number, alpha: number): DisplayObject {
+function getRect(color: number, alpha: number = 1.0): DisplayObject {
   const g = new Graphics();
   g.beginFill(color).drawRect(0, 0, 128, 32).endFill();
   g.alpha = alpha;
