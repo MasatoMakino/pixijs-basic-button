333b61467843d0210c0091f2bddf2b7d26c04bcf
MasatoMakino <makino.masato.g@gmail.com>
2023-03-02 12:00:47 +0900

    add : event listener type

--- src/SelectionState.ts
+++ src/SelectionState.ts
@@ -1,12 +1,12 @@
-import { utils } from "@pixi/core";
+import { EventEmitter } from "@pixi/utils";
 import { BasicButtonContext } from "./BasicButtonContext";
 
 export interface BasicButtonSelectionEventType {
-  selected: BasicButtonContext;
-  unselected: BasicButtonContext;
+  selected: (e: BasicButtonContext) => void;
+  unselected: (e: BasicButtonContext) => void;
 }
 
-export class SelectionState extends utils.EventEmitter<BasicButtonSelectionEventType> {
+export class SelectionState extends EventEmitter<BasicButtonSelectionEventType> {
   public isSelected: boolean = false;
   constructor(isSelected: boolean = false) {
     super();
