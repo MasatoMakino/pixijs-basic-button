d96f4dbb3796debb064ce47a95e7314f3acf34c4
MasatoMakino <makino.masato.g@gmail.com>
2024-08-01 14:46:11 +0900

    migrate : vitest worker to browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,19 +2,16 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    name: "jsdomTest",
-    environment: "jsdom",
-    environmentOptions: {
-      jsdom: {
-        resources: "usable",
-      },
+    browser: {
+      enabled: true,
+      name: "chrome",
+      provider: "webdriverio",
+      headless: true,
     },
-    pool: "forks",
     coverage: {
       provider: "istanbul",
       reporter: ["text", "lcov"],
       include: ["src/**/*.ts"],
     },
-    setupFiles: ["@vitest/web-worker"],
   },
 });
